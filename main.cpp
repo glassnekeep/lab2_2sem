74d163681087b2ddbbdb13288ccee5b3653e61fb
glassnekeep <56785261+glassnekeep@users.noreply.github.com>
2022-05-05 14:07:15 +0300

    Deleted prints and changed them into cout

--- main.cpp
+++ main.cpp
@@ -5,143 +5,183 @@
 using namespace std;
 
 template<class T>
-void input(T* a) {
+void input(T* value) {
     while (true) {
-        cout << "Enter a value:";
-        cin >> *a;
+        //cout << "Enter a value:";
+        cin >> *value;
         break;
     }
 }
 
-void menu() {   int data_type=0;
-    printf("Enter 1 for int, 2 for double\n");
-    while (data_type != 1 && data_type != 2) {
+void menu() {
+    int data_type = 0;
+    //printf("Enter 1 for int, 2 for double\n");
+    cout << "1. int" << endl << "2. double" << endl;
+    do {
         input(&data_type);
-    }
+    } while (data_type != 2 && data_type != 1);
+    /*while (data_type != 1 && data_type != 2) {
+        input(&data_type);
+    }*/
     if (data_type == 1) {
-        printf("Enter 1 for ListSequence, 2 for ArraySequence\n");
+        //printf("Enter 1 for ListSequence, 2 for ArraySequence\n");
+        cout << "1. ListSequence" << endl << "2. ArraySequence" << endl;
         int type = -1;
-        while (type != 1 && type != 2) {
+        /*while (type != 1 && type != 2) {
             input(&type);
-        }
+        }*/
+        do {
+            input(&type);
+        } while (type != 1 && type != 2);
         Sequence<int> *seq;
         if (type == 1) {
             seq = new LinkedListSequence<int>();
         } else {
             seq = new ArraySequence<int>;
         }
-        printf("Enter 1 for append, 2 for prepend, 3 for insertAt,  4 for getLength, 5 for print,6 for get subseq 0 for exit program\n");
+        //printf("Enter 1 for append, 2 for prepend, 3 for insertAt,  4 for getLength, 5 for print,6 for get subseq 0 for exit program\n");
+        cout << "1. Append" << endl << "2. Prepend" << endl << "3. Insert at" << endl << "4. Get length" << endl << "5. Print" << endl << "6. Subsequence" << endl << "0. Exit";
         while (true) {
             type = -1;
             int index;
             while (type < 0 || type > 6) {
-                printf("Enter a command: \n");
+                cout << "Choose option" << endl;
+                //printf("Choose option: \n");
                 input(&type);
             }
+            int item;
             switch (type) {
                 case 0:
                     exit(0);
                     break;
                 case 1:
-                    seq -> append(0);
-                    printf("Enter append item\n");
-                    input(&(*seq)[seq->getLength() - 1]);
+                    //seq -> append(0);
+                    //printf("Enter append item\n");
+                    cout << "Enter appending item" << endl;
+                    //input(&(*seq)[seq -> getLength() - 1]);
+                    input(&item);
+                    seq -> append(item);
                     break;
                 case 2:
-                    seq -> prepend(0);
-                    printf("Enter prepend item\n");
+                    //seq -> prepend(0);
+                    //printf("Enter prepend item\n");
+                    cout << "Enter prepend item" << endl;
+                    input(&item);
+                    seq -> prepend(item);
                     break;
                 case 3:
-                    printf("Enter insert index\n");
+                    //printf("Enter insert index\n");
+                    cout << "Enter insert index" << endl;
                     input(&index);
                     try {
-                        seq -> insertAt(0, index);
-                        printf("Enter insert item\n");
-                    } catch (Exception &abc) {
-                        cout << "Process finished with error code = " << abc.number << endl;
+                        //seq -> insertAt(0, index);
+                        //printf("Enter insert item\n");
+                        cout << "Enter insert item" << endl;
+                        input(&item);
+                        seq -> insertAt(item, index);
+
+                    } catch (Exception& exception) {
+                        cout << "Process finished with error code = " << exception.number << endl;
                         exit(0);
                     }
                     break;
                 case 4:
-                    std::cout << "The length is: " << seq->getLength() << std::endl;
+                    cout << "The length is: " << seq -> getLength() << endl;
                     break;
                 case 5:
                     cout << seq << endl;
                     break;
                 case 6:
-                    int startindex;
-                    int endindex;
-                    printf("Enter start index\n");
-                    input(&startindex);
-                    printf("Enter insert index\n");
-                    input(&endindex);
-                    Sequence<int> *seq2 = seq->getSubsequence(startindex, endindex);
+                    int fromIndex;
+                    int toIndex;
+                    //printf("Enter start index\n");
+                    cout << "Enter start index" << endl;
+                    input(&fromIndex);
+                    //printf("Enter insert index\n");
+                    cout << "Enter end index" << endl;
+                    input(&toIndex);
+                    Sequence<int> *seq2 = seq -> getSubsequence(fromIndex, toIndex);
                     cout << "Subsequence : " << seq2 << endl;
             }
         }
     } else {
-        printf("Enter 1 for ListSequence, 2 for ArraySequence\n");
+        //printf("Enter 1 for ListSequence, 2 for ArraySequence\n");
+        cout << "1. ListSequence" << endl << "2. ArraySequence" << endl;
         int type = -1;
-        while (type != 1 && type != 2) {
+        /*while (type != 1 && type != 2) {
             input(&type);
-        }
+        }*/
+        do {
+            input(&type);
+        } while (type != 1 && type != 2);
         Sequence<double> *seq;
         if (type == 1) {
-            seq = new LinkedListSequence<double>;
+            seq = new LinkedListSequence<double>();
         } else {
             seq = new ArraySequence<double>;
         }
-        printf("Enter 1 for append, 2 for prepend, 3 for insertAt,  4 for getLength, 5 for print,6 for get subseq 0 for exit program\n");
+        //printf("Enter 1 for append, 2 for prepend, 3 for insertAt,  4 for getLength, 5 for print,6 for get subseq 0 for exit program\n");
+        cout << "1. Append" << endl << "2. Prepend" << endl << "3. Insert at" << endl << "4. Get length" << endl << "5. Print" << endl << "6. Subsequence" << endl << "0. Exit";
         while (true) {
             type = -1;
             int index;
             while (type < 0 || type > 6) {
-                printf("Enter a command: \n");
+                cout << "Choose option" << endl;
+                //printf("Choose option: \n");
                 input(&type);
             }
+            double item;
             switch (type) {
                 case 0:
                     exit(0);
                     break;
                 case 1:
-                    seq->append(0);
-                    printf("Enter append item\n");
+                    //seq -> append(0);
+                    //printf("Enter append item\n");
+                    cout << "Enter appending item" << endl;
+                    //input(&(*seq)[seq -> getLength() - 1]);
+                    input(&item);
+                    seq -> append(item);
                     break;
                 case 2:
-                    seq->prepend(0);
-                    printf("Enter prepend item\n");
+                    //seq -> prepend(0);
+                    //printf("Enter prepend item\n");
+                    cout << "Enter prepend item" << endl;
+                    input(&item);
+                    seq -> prepend(item);
                     break;
                 case 3:
-                    printf("Enter insert index\n");
+                    //printf("Enter insert index\n");
+                    cout << "Enter insert index" << endl;
                     input(&index);
                     try {
-                        seq->insertAt(0, index);
-                        printf("Enter insert item\n");
-                    } catch (Exception &ex) {
-                        cout << "Process finished with error code = " << ex.number << endl;
+                        //seq -> insertAt(0, index);
+                        //printf("Enter insert item\n");
+                        cout << "Enter insert item" << endl;
+                        input(&item);
+                        seq -> insertAt(item, index);
+                    } catch (Exception& exception) {
+                        cout << "Process finished with error code = " << exception.number << endl;
                         exit(0);
                     }
                     break;
                 case 4:
-                    cout << "The length is: " << seq->getLength() << std::endl;
+                    cout << "The length is: " << seq -> getLength() << endl;
                     break;
                 case 5:
                     cout << seq << endl;
                     break;
                 case 6:
-                    int startindex;
-                    int endindex;
-                    printf("Enter start index\n");
-                    input(&startindex);
-                    printf("Enter insert index\n");
-                    input(&endindex);
-                    try {
-                        Sequence<double> *seq2 = seq->getSubsequence(startindex, endindex);
-                        cout << "Subsequence : " << seq2 << endl;
-                    } catch (Exception &ex) {
-                        cout << "Process finished with error code = " << ex.number << endl;
-                        exit(0);
-                    }
+                    int fromIndex;
+                    int toIndex;
+                    //printf("Enter start index\n");
+                    cout << "Enter start index" << endl;
+                    input(&fromIndex);
+                    //printf("Enter insert index\n");
+                    cout << "Enter end index" << endl;
+                    input(&toIndex);
+                    Sequence<double> *seq2 = seq -> getSubsequence(fromIndex, toIndex);
+                    cout << "Subsequence : " << seq2 << endl;
             }
         }
     }
